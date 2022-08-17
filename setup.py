c83f076dc12483fab88ad665d3add938331db702
dan-blanco <51581590+dan-blanco@users.noreply.github.com>
2022-08-17 09:17:29 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='time_it',
+    version='1',
+    license='MIT',
+    author="Code Blanco",
+    author_email='email@example.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/CodeBlanco/python-module-time_it',
+    keywords='time it',
+    install_requires=[
+      ],
+
+)
