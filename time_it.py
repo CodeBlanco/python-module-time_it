0bad0932400b64805dbf8447524e5ac2a9e099b9
dan-blanco <51581590+dan-blanco@users.noreply.github.com>
2022-08-16 17:07:12 -0700

    Create time_it.py

---
+++ time_it.py
@@ -0,0 +1,34 @@
+
+
+def time_it(func):
+  """
+    This function is a decorator
+    It will run the function the decorator is applied to, and return its result
+    Printing the execution time
+    
+    eg.
+    
+    @time_it
+    def time_max(A):
+        return max(A)
+        
+    time_max([1,2,3,4,6]) # will return max value and print execution time
+    
+  """
+     
+     # if function takes any arguments,
+     # can be added like this.
+     def inner_def(*args, **kwargs):
+         
+         # storing time before function execution
+         begin = time.time()
+  
+         r = func(*args, **kwargs)
+         
+         # storing time after function execution
+         end = time.time()
+         print(f"{end - begin} time taken in : ", func.__name__, )
+ 
+         return r
+ 
+     return inner_def
